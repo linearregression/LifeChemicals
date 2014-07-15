d21a915a57793a2b51c3d432dde6592d89f8d8b3
root <edward>
2014-07-15 15:31:41 -0700

    chekc in for now

--- transform_sdf_to_rda.R
+++ transform_sdf_to_rda.R
@@ -4,20 +4,30 @@
 ##################
 
 library("ChemmineR")
+library("futile.logging")
 
-stream_input<-function(folder, debug=TRUE){
-   
+layout <- layout.format('[~l] [~t] [~n.~f] ~m')
+flog.logger(DEBUG, appender=appender.file('sdf3rda.log')))
+flog.layout(layout)
+flog.appender(appender.console)
 
+init_environment <- function(){
+   (base.dir <- getwd())
+   if (!is.null(base.dir)) setwd(base.dir) 
 }
 
-sdf_2_rda<-function(files, debug=TRUE) {
-
-    sdfset<-read.SDFset()
-    apset<-sdf2ap(sdfset)
-    save.image(file=gsub(".sdf", "_Image.rda", files[p]), compress=T)
+sdf_2_rda<-function(file=, debug=T) {  
+   flog.info("Reading sdf set") 
+   sdfset<-read.SDFset(file)
+   flog.info("Save as apset set") 
+   apset<-sdf2ap(sdfset)
+   flog.info("Save as rda image") 
+   save.image(file=gsub(".sdf", "_Image.rda", file), compress="bzip2", safe=T)
+   flog.info("Saved %s_Image.rda. File Size", file, size)
 }
 
 upload_file<-function(files, debug=TRUE) {
 
+   files<-list.files(pattern=".sdf", recursive=F)
 }
 
