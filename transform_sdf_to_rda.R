aece02af92622dc8af83af0943081c73b6e6709f
root <edward>
2014-07-15 22:02:19 -0700

    put in some skeleton folder per package.skeleton()

--- transform_sdf_to_rda.R
+++ transform_sdf_to_rda.R
@@ -4,26 +4,34 @@
 ##################
 
 library("ChemmineR")
-library("futile.logging")
+library("futile.logger")
 
-layout <- layout.format('[~l] [~t] [~n.~f] ~m')
-flog.logger(DEBUG, appender=appender.file('sdf3rda.log')))
-flog.layout(layout)
-flog.appender(appender.console)
 sdf_2_rda<-function(file="stdin", debug=F)
 
 init_environment<-function() {
+   layout<-layout.format('[~l] [~t] [~n.~f] ~m')
+   flog.logger(DEBUG, appender=appender.file('sdf3rda.log'))
+   flog.layout(layout)
+   flog.appender(appender.console)
    (base.dir<-getwd())
    if (!is.null(base.dir)) {
       setwd(base.dir)
       flog.info("Current base dir: ~s", base.dir)
-      image.dir<-paste(base.dir,"image",sep="/")
-      if (!file.exists(image.dir)){
-          flog.info("ImageDir not found. Creating ~s", image.dir)
-          file.create(image.dir, showWarnings=T)
-          flog.info("ImageDir ~s Created", image.dir)
+      create_if_absent(base.dir,"image")
+      create_if_absent(base.dir,"log")
+      create_if_absent(base.dir,"data/sdf")
+   }
+}
+
+create_if_absent<-function(basedir, dirname) {
+   if (!is.null(basedir)) {
+      newfile<-paste(basedir, dirname, sep="/")
+      if (!file.exists(newfile)){
+          flog.info("~s not found. Creating ~s", newfile)
+          file.create(newfile, showWarnings=T)
+          flog.info("~s Created", newfile)
       }
-   } 
+   }
 }
 
 get_image_filename<-function(folder="image",file=NULL) {
@@ -34,14 +42,14 @@ get_image_filename<-function(folder="image",file=NULL) {
    return ret
 }
 
-sdf_2_rda <- function(file="stdin", debug=F) {  
+sdf_2_rda <- function(file="stdin", debug=FALSE) {  
    flog.info("Reading sdf set") 
    sdfset<-read.SDFset(file)
    flog.info("Save as apset set") 
    apset<-sdf2ap(sdfset)
    flog.info("Save as rda image") 
    image_file<-get_image_filename(file=gsub(".sdf", "_Image.rda", file))
-   save.image(file=image_file, compress="bzip2", safe=T)
+   save.image(file=image_file, compress="bzip2", safe=TRUE)
    flog.info("Saved %s_Image.rda. File Size", file, size)
 }
 
@@ -49,8 +57,25 @@ upload_file<-function(files, debug=TRUE) {
    files<-list.files(pattern=".rda", recursive=T)
 }
 
-cleanup<-function() {
-  
+get_rda_files<-function() {
+   list_files(cwd=".", extension=".rda")
+}
 
+get_sdf_files<-function(){
+   list_files(cwd=".", extension=".sdf")
+}
+
+list.files<-function (cwd, extension) {
+  ext<-sprintf("\\.%s$",extension)
+  flog.info("Current directory: %s extension: 5s", cwd, ext)
+  fs<-list.files(path=cwd, recursive=TRUE, full.names=TRUE, pattern=ext)
+
+}
+
+cleanup<-function() {
+  rda.files<-list.files(path=getwd(), recursive=T, full.names=T, pattern="\\.rda$")
+  sdf.files<-list.files(path=getwd(), recursive=T, full.names=T, pattern="\\.sdf$")
+  rm(rda.files)
+  rm(sdf.files)
 }
 
