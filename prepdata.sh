acb7b1deb16542ebb07011e788811ba932937e89
root <edward>
2014-07-16 12:48:24 -0700

    add test for parallel presence

--- prepdata.sh
+++ prepdata.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+
+# This script do a parallel unzip of sdf.zip files located in DATADIR to OUTDIR
+# You need to ave parallel installed
+# Only tested unde Ubuntu
+
 echo on
 set -e
 export DATADIR="/home/developer/Dropbox/DataLifeChemicals"
 export OUTDIR="/opt/andrew/data"
+
+command -v parallel >/dev/null 2>&1 || { echo "Require parallel but not installed.  Aborting." >&2; exit 1; }
 
 if [[ -z "$DATADIR" ]]; then
 	echo "Make sure you set \$DATADIR before running this script"
